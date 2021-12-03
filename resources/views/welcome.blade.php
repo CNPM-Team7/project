92e39c07db508b0a35bab73867cb6d067b843e4f
Kami Yuri <dung.hv194254@sis.hust.edu.vn>
2021-12-03 19:13:23 +0700

    add livewire and alpinejs (js framework) add wire-elements/modal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,11 +6,7 @@
 @endsection
 
 @section('content')
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="h-screen bg-white overflow-hidden shadow-xl sm:rounded-lg border border-gray-100 p-10">
-                <h2>Welcome</h2>
-            </div>
-        </div>
+    <div class="">
+        <h2>Welcome</h2>
     </div>
 @endsection

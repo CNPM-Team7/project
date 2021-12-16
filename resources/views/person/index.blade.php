1ce43dc797b573496bdd7e482268e5dab6e4065a
trunghie01 <trunghie01@gmail.com>
2021-12-16 15:37:04 +0700

    merge conflict

--- resources/views/person/index.blade.php
+++ resources/views/person/index.blade.php
@@ -45,18 +45,29 @@
                     <label for="search_type" class="">Lọc theo</label>{{--more search input--}}
                     <select id="search_type"
                             class="w-36 bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                        <option selected>Họ và Tên</option>
-                        <option>Số CMND/CCCD</option>
+                        <option selected value="name">Họ và Tên</option>
+                        <option value="id_number">Số CMND/CCCD</option>
                     </select>
-
+    
                     <label for="search">Tìm kiếm</label>
                     <input
                         class="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                         id="search" type="text" placeholder="Nguyễn Văn A">
-
+                        <button class="border rounded bg-blue-500 px-4 py-2" onclick="search()">Tìm</button>
                 </div>
             </form>
         </div>
+
+        <script>
+            function search(){
+                let searchType = document.getElementById('search_type').value
+                let searchContent = document.getElementById('search').value
+                const urlSearchParams = new URLSearchParams(window.location.search);
+                const params = Object.fromEntries(urlSearchParams.entries());
+                urlSearchParams.set(searchType, searchContent)
+                window.location.search = urlSearchParams.toString()
+            }
+        </script>
 
         <div class="w-full flex flex-row select-none space-x-8 justify-end justify-between px-10">
             <a>
@@ -93,7 +104,7 @@
                             <tr>
                                 <th scope="col"
                                     class="px-6 py-3 text-left text-xs font-medium text-gray-900 uppercase tracking-wider">
-                                    ID Number {{--hover will show register date, place, recieve date, place--}}
+                                    ID Number
                                 </th>
                                 <th scope="col"
                                     class="px-6 py-3 text-left text-xs font-medium text-gray-900 uppercase tracking-wider">
@@ -101,7 +112,7 @@
                                 </th>
                                 <th scope="col"
                                     class="px-6 py-3 text-left text-xs font-medium text-gray-900 uppercase tracking-wider">
-                                    Birthday {{--hover will show birth place--}}
+                                    Birthday
                                 </th>
                                 <th scope="col"
                                     class="px-6 py-3 text-left text-xs font-medium text-gray-900 uppercase tracking-wider">
@@ -117,7 +128,7 @@
                                 </th>
                                 <th scope="col"
                                     class="px-6 py-3 text-left text-xs font-medium text-gray-900 uppercase tracking-wider">
-                                    Job {{--hover will show work place--}}
+                                    Job
                                 </th>
                                 <th scope="col"
                                     class="px-6 py-3 text-left text-xs font-medium text-gray-900 uppercase tracking-wider">
@@ -156,16 +167,16 @@
                                     <div id="tooltip-id_number" role="tooltip" class="tooltip absolute z-10 inline-block bg-gray-900 font-medium shadow-sm text-white py-2 px-3 text-sm rounded-lg opacity-0 duration-300 transition-opacity invisible dark:bg-gray-700">
                                         <div class="flex flex-col">
                                             <span>
-                                                register date
+                                                Register Date: <u>{{ $person->register_date }}</u>
                                             </span>
                                             <span>
-                                                place
+                                                Register Place: <u>{{ $person->register_place }}</u>
                                             </span>
                                             <span>
-                                                recieve date
+                                                Receive Date: <u>{{ $person->idn_receive_date }}</u>
                                             </span>
                                             <span>
-                                                place
+                                                Receive Place: <u>{{ $person->idn_receive_place }}</u>
                                             </span>
                                         </div>
                                         <div class="tooltip-arrow" data-popper-arrow></div>
@@ -184,14 +195,14 @@
                                     </td>
                                     <div id="tooltip-birthday" role="tooltip" class="tooltip absolute z-10 inline-block bg-gray-900 font-medium shadow-sm text-white py-2 px-3 text-sm rounded-lg opacity-0 duration-300 transition-opacity invisible dark:bg-gray-700">
                                         <span>
-                                            birthday place
+                                            Birthday Place: <u>{{ $person->birth_place }}</u>
                                         </span>
                                         <div class="tooltip-arrow" data-popper-arrow></div>
                                     </div>
                                     <td class="px-6 py-4 whitespace-nowrap">
                                         <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full
                                             bg-{{ $genderColors[$person->sex] }}-100 text-{{ $genderColors[$person->sex] }}-500">
-                                            {{ $genders[$person->sex] }} {{--set length--}}
+                                            {{ $genders[$person->sex] }} {{--set fixed length--}}
                                         </span>
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap">
@@ -206,7 +217,7 @@
                                     </td>
                                     <div id="tooltip-job" role="tooltip" class="tooltip absolute z-10 inline-block bg-gray-900 font-medium shadow-sm text-white py-2 px-3 text-sm rounded-lg opacity-0 duration-300 transition-opacity invisible dark:bg-gray-700">
                                         <span>
-                                            work place
+                                            Work Place: <u>{{ $person->work_place }}</u>
                                         </span>
                                         <div class="tooltip-arrow" data-popper-arrow></div>
                                     </div>

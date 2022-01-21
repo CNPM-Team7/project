39697d23e531d27d53bb38475e9f9f2450be3cbf
KamiYuri <dung.hv194254@sis.hust.edu.vn>
2022-01-22 00:23:59 +0700

    fix sth

--- resources/views/authenticate/login.blade.php
+++ resources/views/authenticate/login.blade.php
@@ -4,14 +4,14 @@
     <form action="{{ route('auth.login') }}" method="POST" class="w-full px-7 py-5 flex items-center flex-col">
         @csrf
         <div class="mb-6 flex flex-row w-full">
-            <label for="username" class="w-1/3 text-center align-middle text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Tài
+            <label for="username" class="w-1/3 text-center align-middle text-sm font-medium text-gray-900 block dark:text-gray-300 pr-4">Tài
                 khoản</label>
             <input type="text" id="username" name="username"
                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                    required="">
         </div>
-        <div class="mb-6 flex flex-row w-full">
-            <label for="password" class="w-1/3 text-center align-middle text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Mật
+        <div class="mb-6 flex flex-row w-full items-center">
+            <label for="password" class="w-1/3 text-center align-middle text-sm font-medium text-gray-900 block dark:text-gray-300 pr-4">Mật
                 khẩu</label>
             <input type="password" id="password" name="password"
                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
